004b6ac6c6b6c3006b99e3f766291b1aa19b50e9
joseph.yang <before30@gmail.com>
2024-03-13 22:30:38 +0900

    Delete all
    Delete all

--- backend-service/src/main/kotlin/cc/before30/petclinic/backendservice/owner/PetType.kt
+++
@@ -1,9 +0,0 @@
-package cc.before30.petclinic.backendservice.owner
-
-import cc.before30.petclinic.backendservice.model.NamedEntity
-import jakarta.persistence.Entity;
-import jakarta.persistence.Table;
-
-@Entity
-@Table(name = "types")
-open class PetType : NamedEntity()
--- backend-service/src/main/kotlin/cc/before30/petclinic/backendservice/vet/Specialty.kt
+++
@@ -1,8 +0,0 @@
-package cc.before30.petclinic.backendservice.vet
-
-import cc.before30.petclinic.backendservice.model.NamedEntity
-import jakarta.persistence.Entity
-import jakarta.persistence.Table
-@Entity
-@Table(name = "specialties")
-class Specialty : NamedEntity()
